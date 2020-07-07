012e922c647d805365f9df2c71de88f46a1d10e9
Babul Akhtar <Babul Akhtar@LAPTOP-UVRQB65N>
2020-07-07 19:45:26 +0530

    First Test Commit by Nirmal

---
+++ designpattern/src/designpattern/TestMe.java
@@ -0,0 +1,11 @@
+package designpattern;
+
+public class TestMe {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Design pattern starts...");
+
+	}
+
+}
